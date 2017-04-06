c78ae7446b643451dffd1397e1e4a9f213cbe5d2
Peter Schmalfeldt <peter@manifestinteractive.com>
2017-04-06 11:39:08 -0400

    trying to remember how to set id_ SSH account

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ssh -i id_5e9374f13a50d7e09d315e618d2cfda8 civilservices@192.81.212.49 << EOF
+ssh -i id_cd567023268175d5eef2286540c57939 civilservices@192.81.212.49 << EOF
 
 echo -e "\n\033[38;5;34m✓ Civil Services › Automated Deployment\033[0m\n"
 
